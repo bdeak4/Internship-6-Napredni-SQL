1105e0a252d532391b5fefa7b4bc3fa5dca7b215
Bartol Deak <b@bdeak.net>
2021-12-17 17:06:18 +0100

    add class schema and seed

--- School.sql
+++ School.sql
@@ -28,6 +28,11 @@ CREATE TABLE StudentGroups (
 	CourseId int FOREIGN KEY REFERENCES Courses(Id) NOT NULL,
 )
 
+CREATE TABLE Classes (
+	Id int IDENTITY(1, 1) PRIMARY KEY,
+	Title nvarchar(50) NOT NULL
+)
+
 CREATE TABLE StudentGroupMembers (
 	Id int IDENTITY(1, 1) PRIMARY KEY,
 	StudentGroupId int FOREIGN KEY REFERENCES StudentGroups(Id) NOT NULL,
@@ -38,6 +43,7 @@ CREATE TABLE StudentGroupLectures (
 	Id int IDENTITY(1, 1) PRIMARY KEY,
 	StudentGroupId int FOREIGN KEY REFERENCES StudentGroups(Id) NOT NULL,
 	TeacherId int FOREIGN KEY REFERENCES Teachers(Id) NOT NULL,
+	ClassId int FOREIGN KEY REFERENCES Classes(Id) NOT NULL,
 	StartDate datetime2 NOT NULL,
 	EndDate datetime2 NOT NULL
 )
@@ -95,32 +101,37 @@ INSERT INTO StudentGroups (CourseId) VALUES
 (1),
 (5)
 
+INSERT INTO Classes(Title) VALUES
+('A100'),
+('A101'),
+('C401')
+
 INSERT INTO StudentGroupMembers (StudentGroupId, StudentId) SELECT 1, id FROM Students WHERE Birthday LIKE '198[0-4]%' AND PriorKnowledge = 'A2'
 INSERT INTO StudentGroupMembers (StudentGroupId, StudentId) SELECT 2, id FROM Students WHERE Birthday LIKE '198[5-9]%' AND PriorKnowledge = 'B2'
 INSERT INTO StudentGroupMembers (StudentGroupId, StudentId) SELECT 3, id FROM Students WHERE Birthday LIKE '199[0-5]%' AND PriorKnowledge = 'B2'
 INSERT INTO StudentGroupMembers (StudentGroupId, StudentId) SELECT 4, id FROM Students WHERE Birthday LIKE '198[5-9]%' AND PriorKnowledge = 'B2'
 
 -- https://www.mockaroo.com/
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (4, 1, '2021-02-26 17:00:00', '2021-02-26 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 1, '2021-05-17 17:00:00', '2021-05-17 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 9, '2021-12-10 17:00:00', '2021-12-10 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (2, 5, '2021-04-12 17:00:00', '2021-04-12 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 2, '2021-04-05 17:00:00', '2021-04-05 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (2, 9, '2021-09-30 17:00:00', '2021-09-30 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 10, '2021-04-26 17:00:00', '2021-04-26 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 8, '2021-09-25 17:00:00', '2021-09-25 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 1, '2021-03-14 17:00:00', '2021-03-14 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (1, 2, '2021-06-11 17:00:00', '2021-06-11 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (4, 6, '2021-07-05 17:00:00', '2021-07-05 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 6, '2021-06-12 17:00:00', '2021-06-12 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (3, 9, '2021-12-03 17:00:00', '2021-12-03 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (1, 6, '2021-11-28 17:00:00', '2021-11-28 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (4, 6, '2021-09-09 17:00:00', '2021-09-09 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (1, 7, '2021-12-04 17:00:00', '2021-12-04 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (2, 9, '2021-07-10 17:00:00', '2021-07-10 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (1, 6, '2021-03-24 17:00:00', '2021-03-24 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (2, 6, '2021-08-21 17:00:00', '2021-08-21 20:00:00');
-insert into StudentGroupLectures (StudentGroupId, TeacherId, StartDate, EndDate) values (4, 10, '2021-10-25 17:00:00', '2021-10-25 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (4, 1, 3, '2021-02-26 17:00:00', '2021-02-26 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 1, 3, '2021-05-17 17:00:00', '2021-05-17 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 9, 3, '2021-12-10 17:00:00', '2021-12-10 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (2, 5, 3, '2021-04-12 17:00:00', '2021-04-12 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 2, 3, '2021-04-05 17:00:00', '2021-04-05 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (2, 9, 3, '2021-09-30 17:00:00', '2021-09-30 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 10, 1, '2021-04-26 17:00:00', '2021-04-26 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 8, 2, '2021-09-25 17:00:00', '2021-09-25 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 1, 2, '2021-03-14 17:00:00', '2021-03-14 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (1, 2, 2, '2021-06-11 17:00:00', '2021-06-11 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (4, 6, 2, '2021-07-05 17:00:00', '2021-07-05 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 6, 2, '2021-06-12 17:00:00', '2021-06-12 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (3, 9, 2, '2021-12-03 17:00:00', '2021-12-03 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (1, 6, 2, '2021-11-28 17:00:00', '2021-11-28 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (4, 6, 2, '2021-09-09 17:00:00', '2021-09-09 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (1, 7, 2, '2021-12-04 17:00:00', '2021-12-04 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (2, 9, 2, '2021-07-10 17:00:00', '2021-07-10 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (1, 6, 2, '2021-03-24 17:00:00', '2021-03-24 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (2, 6, 2, '2021-08-21 17:00:00', '2021-08-21 20:00:00');
+insert into StudentGroupLectures (StudentGroupId, TeacherId, ClassId, StartDate, EndDate) values (4, 10, 1, '2021-10-25 17:00:00', '2021-10-25 20:00:00');
 
 INSERT INTO StudentGroupLectureAttendance (StudentGroupLectureId, StudentId, Present)
 SELECT l.Id, m.StudentId, CRYPT_GEN_RANDOM(1) % 2
